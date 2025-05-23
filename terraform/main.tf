a3d13285dde33b2a9e05d003f7ce5ee40f0114bc
Sachin-960 <dayalsachin0@gmail.com>
2025-05-23 16:59:55 +0530

    v1

--- terraform/main.tf
+++ terraform/main.tf
@@ -37,7 +37,7 @@ resource "aws_security_group" "node_app_sg" {
 }
 
 resource "aws_instance" "node_app_server" {
-  ami           = "ami-0c55b159cbfafe1f0" # Amazon Linux 2
+  ami           = "ami-0953476d60561c955" # Amazon Linux 
   instance_type = "t2.micro"
 
   key_name          = var.key_pair_name
