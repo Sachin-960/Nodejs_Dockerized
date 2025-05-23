07377d9240e73648a3521b1d623b64d660caf0f2
Sachin-960 <dayalsachin0@gmail.com>
2025-05-23 17:23:46 +0530

    v1

--- terraform/main.tf
+++ terraform/main.tf
@@ -50,7 +50,7 @@ locals {
 }
 
 resource "aws_instance" "node_app_server" {
-  ami           = "ami-0c55b159cbfafe1f0" # Amazon Linux 2
+  ami           = "ami-0953476d60561c955" # Amazon Linux 
   instance_type = "t2.micro"
 
   key_name          = var.key_pair_name
